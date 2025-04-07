84999e89c78149872fd61c9f078578c1cd75a7ad
AlexBand-Pro <tillromutni@gmail.com>
2025-04-07 16:48:45 -0600

    add change

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext' // 👈 Enables top-level await support
+    target: 'esnext'
   }
 })
